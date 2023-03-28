e94d231b6536093f4fc655e7c85810509419591b
Sujithraj22 <sujithraj@benagon.com>
2023-03-28 09:52:54 +0530

    working on icons

---
+++ lib/models/work.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Work {
+  const Work({this.title, this.icon});
+
+  final String? title;
+  final IconData? icon;
+}
+
+List<Work> works = <Work>[
+  const Work(title: 'Medication', icon: Icons.medication_rounded),
+  const Work(title: 'Body Map', icon: Icons.man_2_rounded),
+  const Work(title: 'Food', icon: Icons.map),
+  const Work(title: 'Drinks', icon: Icons.phone),
+  const Work(title: 'Personal care', icon: Icons.camera_alt),
+  const Work(title: 'Repositioning', icon: Icons.settings),
+  const Work(title: 'Companionship / respite care', icon: Icons.photo_album),
+  const Work(title: 'Laundry', icon: Icons.wifi),
+  const Work(title: 'Groceries', icon: Icons.wifi),
+  const Work(title: 'Housework', icon: Icons.wifi),
+  const Work(title: 'Household chores', icon: Icons.wifi),
+  const Work(title: 'Unable to delivere care', icon: Icons.wifi),
+];
+
+class SelectWork extends StatelessWidget {
+  const SelectWork({Key? key, required this.work}) : super(key: key);
+  final Work work;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle =
+    TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+
+              Icon(work.icon, size: 50.0, color: Colors.black),
+              SizedBox(height: 10),
+              Text((work.title).toString(), style: textStyle),
+            ]),
+      ),
+    );
+  }
+}
--- lib/screens/visiting_screen.dart
+++ lib/screens/visiting_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/work.dart';
+
 class VisitingScreen extends StatefulWidget {
   const VisitingScreen({Key? key}) : super(key: key);
 
@@ -36,42 +38,6 @@ class _VisitingScreenState extends State<VisitingScreen> {
   }
 }
 
-class Work {
-  const Work({this.title, this.icon});
 
-  final String? title;
-  final IconData? icon;
-}
 
-List<Work> works = <Work>[
-  const Work(title: 'Home', icon: Icons.home),
-  const Work(title: 'Contact', icon: Icons.contacts),
-  const Work(title: 'Map', icon: Icons.map),
-  const Work(title: 'Phone', icon: Icons.phone),
-  const Work(title: 'Camera', icon: Icons.camera_alt),
-  const Work(title: 'Setting', icon: Icons.settings),
-  const Work(title: 'Album', icon: Icons.photo_album),
-  const Work(title: 'WiFi', icon: Icons.wifi),
-];
 
-class SelectWork extends StatelessWidget {
-  const SelectWork({Key? key, required this.work}) : super(key: key);
-  final Work work;
-
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle textStyle =
-        TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
-    return Card(
-      color: Colors.orange,
-      child: Center(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(child: Icon(work.icon, size: 50.0, color: Colors.white)),
-              Text((work.title).toString(), style: textStyle),
-            ]),
-      ),
-    );
-  }
-}
